a2de3d53c3d7fdcc1e24b57eacf67f2e455f0f5b
tendol <tgyalpo@middlebury.edu>
2020-11-23 22:34:46 -0500

    pretokenized the data

--- preProcessing/sentencePiece.py
+++ preProcessing/sentencePiece.py
@@ -3,9 +3,9 @@ import os
 from tokenizers import SentencePieceBPETokenizer
 
 spm.SentencePieceTrainer.train(
-        input='../data/boMonoData.txt', 
-        model_prefix='m', 
-        vocab_size=32000)
+        input='../data/boTokenData.txt', 
+        model_prefix='m3', 
+        vocab_size=35000)
 
-sp = spm.SentencePieceProcessor(model_file='m.model')
+sp = spm.SentencePieceProcessor(model_file='m3.model')
 print(sp.encode(['ངའི་མིང་ལ་བསྟན་སྒྲོལ་མ་ཟེར་'], out_type=str))
