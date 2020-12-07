917050a76ad997d95fae01f13a9dc3f1fe55c944
tendol <tgyalpo@middlebury.edu>
2020-12-07 00:40:23 -0500

    storing for now to checking lambus init

--- data/s.py
+++ data/s.py
@@ -1,3 +1,5 @@
+
+
 # load doc into memory
 def load_doc(filename):
     # open the file as read only
@@ -12,9 +14,18 @@ def load_doc(filename):
 def to_sentences(doc):
     return doc.strip().split('\n')
 
+def cal_longest_sentence(lines):
+    longest = 0
+    for line in lines:
+        a = line.split("་")
+        if longest < len(a):
+            longest = len(a)
+    return longest
 
-doc = load_doc("boMonoData.txt")
-sentences = to_sentences(doc)
+if __name__ == '__main__':
+    doc = load_doc("train.en")
+    sentences = to_sentences(doc)
+    l = cal_longest_sentence(sentences)
+    print(l) 
 
-for i in range(100000, 110000):
-    print(sentences[i])
+
