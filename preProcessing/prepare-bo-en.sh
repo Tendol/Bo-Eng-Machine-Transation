44d41e2b6dde52dd649d74c854ffee67e37b79dd
tendol <tgyalpo@middlebury.edu>
2020-12-20 18:02:21 -0500

    fairseq bash script

--- preProcessing/prepare-bo-en.sh
+++ preProcessing/prepare-bo-en.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Copyright (c) Facebook, Inc. and its affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
 
 src=bo
 tgt=en
@@ -19,14 +14,14 @@ DATA=$ROOT/data.bo.en.bpe16k
 mkdir -p "$DATA"
 
 TRAIN_MINLEN=1  # remove sentences with <1 BPE token
-TRAIN_MAXLEN=250  # remove sentences with >250 BPE tokens
+TRAIN_MAXLEN=250  # remove sentences with >500 BPE tokens
 
 # prepare train, valid and test data 
 echo "prepare train, valid, and test data"
 cd $ORIG 
 for l in $src $tgt; do
-    sed -n 1,100p data.$l > ../$DATA/test.$lang.$l 
-    sed -n 100,10000p data.$l > ../$DATA/valid.$lang.$l 
+    sed -n 1,1000p data.$l > ../$DATA/test.$lang.$l 
+    sed -n 1000,10000p data.$l > ../$DATA/valid.$lang.$l 
     tail -n +10000 data.$l > ../$DATA/train.$lang.$l 
 done
 cd ..
@@ -34,7 +29,6 @@ cd ..
 # learn BPE with sentencepiece
 TRAIN_FILES=$(for SRC in src; do echo $DATA/train.$lang.${src}; echo $DATA/train.$lang.${tgt}; done | tr "\n" ",")
 echo "learning joint BPE over ${TRAIN_FILES}..."
-
 python "$SPM_TRAIN" \
     --input=$TRAIN_FILES \
     --model_prefix=$DATA/sentencepiece.bpe \
@@ -44,7 +38,6 @@ python "$SPM_TRAIN" \
 
 # encode train, valid, test data 
 echo "encoding train, valid, test with learned BPE..."
-
 for i in "valid" "train" "test"; do 
     python "$SPM_ENCODE" \
         --model "$DATA/sentencepiece.bpe.model" \
